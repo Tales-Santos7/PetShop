f32f3bd55547a6b78a179d169473e5f9c17fc2e3
Tales Santos <detysantosos61@gmail.com>
2026-04-03 22:12:07 -0300

    Upgrade files

--- backend/server.js
+++ backend/server.js
@@ -5,23 +5,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  const allowedOrigins = ["https://petshop-painel.vercel.app", "https://petshop-demo.vercel.app"];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 
